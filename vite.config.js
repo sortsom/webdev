db73c76ef6e2320612826284868107627833fea9
sortsom <62197355+sortsom@users.noreply.github.com>
2025-10-08 17:22:19 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+    server: {
+        https: true
+    },
+    plugins: [laravel(['resources/css/app.css', 'resources/js/app.js'])],
 });
